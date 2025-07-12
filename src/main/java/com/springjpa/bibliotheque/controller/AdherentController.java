9af023d4f83c84c27cdf269e9763ad1977bd950b
01TB <tsitohaina.berthin@gmail.com>
2025-07-12 19:11:53 +0300

    Page adhérent

---
+++ src/main/java/com/springjpa/bibliotheque/controller/AdherentController.java
@@ -0,0 +1,27 @@
+package com.springjpa.bibliotheque.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springjpa.bibliotheque.entity.Adherent;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/adherent")
+public class AdherentController {
+
+    @GetMapping
+    public String preter(HttpSession session, Model model) {
+        Adherent adherent = (Adherent)session.getAttribute("adherent");
+        if(adherent==null){
+            model.addAttribute("message", "Tentative d'attaque");
+            return "redirect:/";
+        }
+
+        return "adherent/home";
+    }
+
+}
